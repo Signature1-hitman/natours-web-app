6bda3174627f40f310e74a9111928dbc78465f40
unknown <shivanshkumar166@gmail.com>
2020-07-26 21:42:36 +0530

    duplicate fields db error controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,6 +4,14 @@ const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(value);
+  
+    const message = `Duplicate field value: ${value}. Please use another value!`;
+    return new AppError(message, 400);
+  };
+  
 module.exports=(err,req,res,next)=>{
   
     err.statusCode=err.statusCode||500
