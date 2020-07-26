df51c4ba059e61319a07e50f0e64a45cbbb43d39
unknown <shivanshkumar166@gmail.com>
2020-07-26 21:47:46 +0530

    Db validation error controller function

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -12,7 +12,10 @@ const handleDuplicateFieldsDB = err => {
     return new AppError(message, 400);
   };
   const handleValidationErrorDB = err => {
-  
+    const errors = Object.values(err.errors).map(el => el.message);
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
   };
 
 module.exports=(err,req,res,next)=>{
