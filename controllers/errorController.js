67ce4a8ac5fb90d783b090179929d0da3683f50a
unknown <shivanshkumar166@gmail.com>
2020-07-26 21:50:11 +0530

    Jwt expire handle function

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -17,7 +17,20 @@ const handleDuplicateFieldsDB = err => {
     const message = `Invalid input data. ${errors.join('. ')}`;
     return new AppError(message, 400);
   };
+  const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
 
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
 module.exports=(err,req,res,next)=>{
   
     err.statusCode=err.statusCode||500
